e66c1706a36154bf2c46d739a5ad190a4bb9d422
hyeonpearl <hc9064@gmail.com>
2023-01-18 15:54:39 +0900

    Style: TodoInsert CSS 작업

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
   return (
     <>
-      <TodoTemplate>Todo App을 만들자!</TodoTemplate>
+      <TodoTemplate>
+        <TodoInsert />
+      </TodoTemplate>
     </>
   );
 };
